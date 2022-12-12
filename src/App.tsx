3115a2c1bfa2abf0cfd6fb7079bf12558a204d7e
BerezinaEV <100429742+BerezinaEV@users.noreply.github.com>
2022-12-12 19:32:04 +0700

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Component} from "./components/Component";
+
+function App() {
+  return <Component text={"Привет!!!♣ здесь очень длинный текст —♥♥♥ текст текст"}></Component>
+}
+
+export default App;
